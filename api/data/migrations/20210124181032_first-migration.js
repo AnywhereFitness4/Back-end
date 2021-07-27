5c962ecd76b9666de098db28c09784f93a0af027
Chaz Carbis <chazcarbis@gmail.com>
2021-07-27 13:44:05 -0500

    made db

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,67 @@
-exports.up = async (knex) => {
-  await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
-      users.timestamps(false, true)
+//Setup the main database schema
+
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('users', (tbl) => {
+      tbl.increments()
+      tbl.string('name').notNullable()
+      tbl.string('email').notNullable().unique()
+      tbl.string('username').notNullable().unique()
+      tbl.string('password').notNullable()
+      tbl.string('role').notNullable()
+      tbl.timestamp('created_at').defaultTo(knex.fn.now())
+    })
+    .createTable('classes', (tbl) => {
+      tbl.increments()
+      tbl.string('name').notNullable()
+      tbl.string('type').notNullable()
+      tbl.string('date_time').notNullable()
+      tbl.string('duration').notNullable()
+      tbl.string('intensity').notNullable()
+      tbl.string('location').notNullable()
+      tbl.integer('max_size').unsigned().notNullable()
+      tbl.timestamp('created_at').defaultTo(knex.fn.now())
+    })
+    .createTable('instructors_classes', (tbl) => {
+      tbl
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('users.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      tbl
+        .integer('classes_id')
+        .unsigned()
+        .notNullable()
+        .references('classes.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      tbl.primary(['user_id', 'classes_id'])
+    })
+    .createTable('attendees', (tbl) => {
+      tbl
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('users.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      tbl
+        .integer('classes_id')
+        .unsigned()
+        .notNullable()
+        .references('classes.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      tbl.primary(['user_id', 'classes_id'])
     })
 }
 
-exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('attendees')
+    .dropTableIfExists('instructors_classes')
+    .dropTableIfExists('classes')
+    .dropTableIfExists('users')
 }
--- api/server.js
+++ api/server.js
@@ -1,29 +1,21 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
-const db = require('./data/db-config')
+//* Import Express and setup server
+const express = require("express");
+const server = express();
 
-function getAllUsers() { return db('users') }
+//* Ensure Express is able to parse JSON data
+server.use(express.json());
 
-async function insertUser(user) {
-  // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-  // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-  // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
-  const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
-  return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-}
+//* Import and setup CORS
+const cors = require("cors");
+server.use(cors());
 
-const server = express()
-server.use(express.json())
-server.use(helmet())
-server.use(cors())
+//* Import Routers
+const usersRouter = require("./users/users-router");
+const classesRouter = require("./classes/classes-router");
 
-server.get('/api/users', async (req, res) => {
-  res.json(await getAllUsers())
-})
+//* Setup Routers
+server.use("/api/users", usersRouter);
+server.use("/api/classes", classesRouter);
 
-server.post('/api/users', async (req, res) => {
-  res.status(201).json(await insertUser(req.body))
-})
-
-module.exports = server
+//* Export server
+module.exports = server;
